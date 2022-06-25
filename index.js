f176945c86930523bdf20ff2b82cb17cc8abaaa4
Priyanshu Patel <76871235+PriyanshuPatel02@users.noreply.github.com>
2022-06-25 10:08:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+
+    function checkloveperc(){
+         var lovedata = (Math.random() * 100+1); 
+         lovedata = Math.floor(lovedata); 
+         document.getElementById('loveValue').value = lovedata + "%";
+    }
